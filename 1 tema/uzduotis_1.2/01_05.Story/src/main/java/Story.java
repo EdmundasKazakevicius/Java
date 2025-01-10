a653f6eaaf31c779cb11c047fb08fbdc68b610a7
Edmundas <edmundas.kazakevicius@stud.techin.lt>
2025-01-10 09:11:40 +0200

    1.2 uzduotis patikslinta

--- 1 tema/uzduotis_1.2/01_05.Story/src/main/java/Story.java
+++ 1 tema/uzduotis_1.2/01_05.Story/src/main/java/Story.java
@@ -7,6 +7,17 @@ public class Story {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("I will tell you a story, but I need some information first.");
 
+        System.out.println("What is the main character called?");
+        String charactersName = scanner.nextLine();
+
+        System.out.println("What is their job?");
+        String charactersJob = scanner.nextLine();
+
+        System.out.println("Here is the story:");
+        System.out.println("Once upon a time there was " + charactersName + ", who was " + charactersJob + ".");
+        System.out.println("On the way to work, " + charactersName + " reflected on life.");
+        System.out.println("Perhaps " + charactersName + " will not be " + charactersJob + " forever.");
     }
 }
